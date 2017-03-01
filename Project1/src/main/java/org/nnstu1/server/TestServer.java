c9e53f63b16cf071806e912ea0ef37f77335c919
Ильдус Каюмов <knifenn@yandex.ru>
2017-03-01 20:17:11 +0300

    Branch test

---
+++ Project1/src/main/java/org/nnstu1/server/TestServer.java
@@ -0,0 +1,21 @@
+package org.nnstu1.server;
+
+import org.nnstu.contract.AbstractServer;
+
+public class TestServer extends AbstractServer{
+
+    public TestServer() {
+        super(8080);
+    }
+
+    @Override
+    public void launchServer() {
+        System.out.println("lul");
+    }
+
+    @Override
+    public void stopServer() throws Exception {
+        System.out.println("kek");
+    }
+
+}
