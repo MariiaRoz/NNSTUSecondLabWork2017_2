cfb420145a8ea508cb29997715aef6c83dc5749e
MaxGrdv <zenitlike@gmail.com>
2017-04-22 19:56:42 +0300

    added new remote interface

--- Project6/src/main/java/org/nnstu6/server/ServerPerformance.java
+++ Project6/src/main/java/org/nnstu6/server/ServerPerformance.java
@@ -3,63 +3,51 @@ package org.nnstu6.server;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.List;
+import java.util.UUID;
 
-     /*
-    It's interface which using client and implements of server,remote service in RMI
-     */
+/*
+It's remote interface
+*/
 public interface ServerPerformance extends Remote {
 
 
     /**
-     *A method that allows you to receive messages and save them in a list
+     * Method that allows you to receive messages
      *
-     * @param message - content of message
-     * @param sender - who send the  message
+     * @param message   - content of message
+     * @param sender    - who send the  message
      * @param recipient - who get the message
-     * @param time -the moment when  you receive a new message
+     * @param time      -the moment when  you receive new message
      * @return - list of messages
      */
-    List getData(String message, String sender,String recipient, int time) throws RemoteException;
+    List<Messages> getMessages(String message, String sender, String recipient, long time) throws RemoteException;
 
     /**
-     *A method that allows you to send messages and issue an error if failed to implement this
+     * Method that allows you to send message
      *
-     * @param message- content of message
-     * @param sender -who send the  message
-     * @param recipient- who get the message
-     * @param time -the moment when  you receive a new message
-     * @return -true, if the message is sent, else false
+     * @param message-  content of message
+     * @param sender    -who send the  message
+     * @param recipient - who get the message
+     * @param time      -the moment when  you send  new message
+     * @return -true, if the message was sent successfully
      */
-    boolean sendData(String message, String sender, String recipient,int time) throws RemoteException;
+    boolean sendMessage(String message, String sender, String recipient, long time) throws RemoteException;
 
     /**
-     *A method that allows the client to create a new dialogue/conversation
+     * Method that allows you to create a new dialogue/conversation
      *
-     * @param key -unique identifier for history of BD
-     * @return - true, if a new conversation is created, otherwise false
+     * @return - special key of dialog
      */
-    boolean creatingNewDialog(String key) throws RemoteException;
+    UUID creatingNewDialog() throws RemoteException;
+
 
     /**
-     * Method that validates the user-entered login and password
+     * Method that allows checking login and password
      *
-     * @param login - login of user
-     * @param password - password of user
-     * @return 2, if failed to login
-     * 1, login and password are correct
-     * 3, if failed in password
-     * 0, unexpected error
-     */
-    int authorization(String login, String password) throws RemoteException;
-
-    /**
-     *A method that allows a new user to register
-     *
-     * @param login - login of user
+     * @param login    - login of user
      * @param password - password of user
      * @return true, if check-in was successful
-     * false,if failed to register
      */
-    boolean registration(String login, String password) throws RemoteException;
+    boolean authorization(String login, UUID password) throws RemoteException;
 
 }
